85cb455b2237d323dbeaf65d028ff315898e0676
Geoffroy Periot <geoffroy.periot@wildcodeschool.fr>
2021-12-03 16:44:47 +0100

    added needed middleware for json parsing and url encoded data

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
