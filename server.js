4e54284e8690567cdff70935259f15618d8e98b3
Geoffroy Periot <geoffroy@pop-os.localdomain>
2021-12-11 18:43:12 +0100

    prisma connection to db ok

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+require('./routes')(app);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Oops, something broke!')
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
